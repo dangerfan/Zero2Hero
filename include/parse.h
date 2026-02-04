307782982420c70ae07b5340e377467d6125c266
dangerfan <jared.dziedzic@gmail.com>
2026-02-03 21:29:09 -0500

    Commit for testing

--- include/parse.h
+++ include/parse.h
@@ -21,4 +21,5 @@ int validate_db_header(int fd, struct dbheader_t **headerOut);
 int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
 int output_file(int fd, struct dbheader_t *);
 
+
 #endif
